5db301a995a29bd4cea42a2784e118ecca89a0db
Gourav <gorv.sehgal@gmail.com>
2025-03-29 14:33:42 -0400

    cra and Tailwind

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-3xl text-bold text-green-800"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Namste React
         </a>
       </header>
     </div>
